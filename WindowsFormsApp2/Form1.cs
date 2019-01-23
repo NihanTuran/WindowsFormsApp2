c2e6db831dc15220d5150927e5d5b2cb17a68bb0
Nihan Turan <nhnturan@gmail.com>
2019-01-23 16:33:24 +0300

    Mesaj butona bağlandı.

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -19,6 +19,11 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             MessageBox.Show("Merhaba Erkan");
         }
     }
